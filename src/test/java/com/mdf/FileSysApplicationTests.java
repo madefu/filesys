3a7a612a2718d517c83376cd5c27b5c149f1d2e4
madefu <madefu_job@126.com>
2020-06-06 15:36:32 +0800

    # WARNING: head commit changed in the meantime
    111

---
+++ src/test/java/com/mdf/FileSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
